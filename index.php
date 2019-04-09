cbbe782d4cb570e3fa979129fe5afa028474da62
YounesAoquab <48790331+YounesAoquab@users.noreply.github.com>
2019-04-09 11:21:30 +0200

    update

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-  background-position: center;
     <link rel="stylesheet" href="./style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AMO-Site</title>   
@@ -10,7 +9,7 @@
 
     <div class="parallax"></div>
 
-    <div style="height:1000px;background-color:red;font-size:36px">
+    <div class="text">
     Scroll Up and Down this page to see the parallax scrolling effect.
     This div is just here to enable scrolling.
     Tip: Try to remove the background-attachment property to remove the scrolling effect.
